5ba422b0b33d8fd621c51bd35f19bba6c4205a24
jahnavichelamkuri <jahnavichelamkuri@gmail.com>
2023-06-12 22:13:21 +0530

    updated models.py

--- task_app/models.py
+++ task_app/models.py
@@ -5,8 +5,8 @@ class Task(models.Model):
     user =models.ForeignKey(User, on_delete=models.CASCADE, null=True,blank=True)
     title = models.CharField(max_length=200)
     description =models.TextField(null=True, blank=True)
+    date =models.DateField(null=True)
     complete =models.BooleanField(default=False)
-    created =models.DateTimeField(auto_now_add=True)
 
 
 # def __str__(self):
